62fc9715bf2ed6a5f76d0fd37773b3becb31fd75
LAPTOP-UOADHCKT\Pedro <pedroankersmituni@gmail.com>
2024-05-08 19:11:28 +0200

    Completado el ejercicio de fumadores

--- fumadoresV2/semaforos/Mesa.java
+++ fumadoresV2/semaforos/Mesa.java
@@ -23,63 +23,38 @@ public class Mesa {
 		}	
 		return nombres;
 	}
-
 	private Semaphore mutex = new Semaphore(1);
-	private Semaphore hayIngredientes = new Semaphore(0);
-	private Semaphore puedoFumar = new Semaphore(0);
-	private Semaphore esperaAgente = new Semaphore(0);
-	private Semaphore puedoPoner = new Semaphore(1);
+	//Habrá un semáforo para cada fumador, que le permita o no pasar dependiendo de los ingredientes que tenga
+	private Semaphore esperaPapelTabaco = new Semaphore(0);
+	private Semaphore esperaPapelCerillas = new Semaphore(0);
+	private Semaphore esperaTabacoCerillas = new Semaphore(0);
+	//Tenemos que controlar que el agente no siga poniendo ingredientes todo el tiempo
+	private Semaphore esperaAgente = new Semaphore(1);
 
 	//El agente llama a este metodo para poner en la mesa dos ingredientes de los tres
 	public void nuevosIngredientes(int ingr1, int ingr2) throws InterruptedException{
-		//mutex para controlar los ingredientes
-
+		esperaAgente.acquire(); //Semáforo de tipo 1, deja pasar y se pone a rojo
 		mutex.acquire();
-		for (int i = 0; i < ingr.length; i++) {
-			ingr[i] = false;
-		}
-		ingr[ingr1] = true;
-		ingr[ingr2] = true;
-		/*int[] a = {0,1,2};
-		a[ingr1] = 0;
-		a[ingr2] = 0;
-		int x = 0;
-		for(int i = 0; i < a.length; i++){
-			if(a[i] != 0){
-				x = i;
-			}
-		}*/
-		String[] n = ingredientes();
-		System.out.println("El agente pone en la mesa " + n[ingr1] + " y " + n[ingr2]);
-		mutex.release();
-		hayIngredientes.release();
-		hayIngredientes.release();
-		hayIngredientes.release();
-		esperaAgente.acquire();
-		mutex.acquire();
-		for (int i = 0; i < ingr.length; i++) {
-			ingr[i] = false;
+		ingr[ingr1]=true;
+		ingr[ingr2]=true;
+		System.out.println("En la mesa hay " + java.util.Arrays.toString(ingredientes()));
+		//Controlamos qué fumador puede fumar
+		if(ingr[0] && ingr[1]){ //Si tenemos tabaco y papel
+			esperaPapelTabaco.release();
+		}else if(ingr[1] && ingr[2]){ //Si tenemos papel y cerillas
+			esperaPapelCerillas.release();
+		} else { //Si tenemos tabaco y cerillas
+			esperaTabacoCerillas.release();
 		}
 		mutex.release();
-		for(int i = 0; i < 3; i++){
-			puedoFumar.release();
-		}
-
 	}
-
 	//El fumador id quiere los dos ingredientes que le faltan para poder fumar
 	public void quieroFumar(int id) throws InterruptedException{
-
-		//El fumador se para en seco hasta que haya ingredientes en la mesa.
-		hayIngredientes.acquire();
-
-		mutex.acquire();
-		boolean aux = ingr[id];
-		mutex.release();
-
-		if(aux){
-			System.out.println("El fumador " + id + "no puede fumar y se bloquea");
-			puedoFumar.acquire();
+	//El fumador se para en seco hasta que haya ingredientes en la mesa.
+		switch (id){ //Dependiendo de qué tipo de fumador sea, le paramos en un semáforo o en otro
+			case 0: esperaPapelCerillas.acquire(); break;
+			case 1: esperaTabacoCerillas.acquire(); break;
+			case 2: esperaPapelTabaco.acquire(); break;
 		}
 
 	}
@@ -87,12 +62,11 @@ public class Mesa {
 	//El fumador id indica que ha terminado de fumar
 	public void finFumar(int id) throws InterruptedException{
 		mutex.acquire();
-		boolean aux = ingr[id];
-		mutex.release();
-		if(!aux){
-			puedoPoner.release();
-			esperaAgente.release();
+		for(int i = 0; i <3; i++){ //Nos aseguramos de que ya no quedan ingredientes
+			ingr[i] = false;
 		}
+		esperaAgente.release(); //Liberamos al agente
+		mutex.release();
 
 	}
 }
